6dba8187c66fb39f3a297195779684968856bb7d
Daniel <danielima.projex@gmail.com>
2025-09-29 09:58:19 -0400

    feat: add homepage background

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,11 +2,11 @@
 import { Button } from '@/components/ui/button';
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import styles from "./Home.module.css"
 import { Hexagon } from 'lucide-react';
 import { DrawerComponent } from '@/components/shared/DrawerComponent/DrawerComponent';
 import { RegisterForm } from '@/components/core/RegisterForm/RegisterForm';
 import { useAuthStore } from '@/states/AuthState';
+import Silk from '@/components/Silk';
 
 type HomeProps = {
   openLogin: () => void
@@ -30,12 +30,20 @@ export function Home({openLogin}: HomeProps) {
 
   return (
 
-      <div className={`w-11/12 h-10/12 mx-auto mt-7 border-2 rounded-2xl ${styles.container}`}>
+      <div className={`w-11/12 h-10/12 mx-auto mt-7 border-2 rounded-2xl relative text-white`}>
+        <div className="absolute w-full h-full -z-10 rounded-2xl overflow-hidden">
+          <Silk
+            speed={5}
+            scale={1}
+            color="#7B7481"
+            noiseIntensity={1.5}
+            rotation={0}
+          />
+        </div>
         <DrawerComponent title="Registre-se" subtitle="Informe seus dados para prosseguir" open={showRegister} onOpenChange={setShowRegister}>
           <RegisterForm  openLogin={onOpenLogin} />
         </DrawerComponent>
-        <div className={styles.stars}></div>
-        <div className="w-full h-full flex flex-col items-center justify-center gap-5 px-4">
+        <div className="w-full h-full flex flex-col items-center justify-center gap-5 px-4 z-10">
           <div className="flex items-center gap-2 text-2xl">
             <h1 className='font-bold'>Study</h1>
             <Hexagon />
