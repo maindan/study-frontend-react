be1f1edfc0bcf995b4b07052a5103d5c430f5637
Daniel <danielima.projex@gmail.com>
2025-09-30 08:46:03 -0400

    feat: add SplitText component and enhance Tasks page with dynamic content and styling

--- src/components/core/Header/Header.tsx
+++ src/components/core/Header/Header.tsx
@@ -15,22 +15,27 @@ export function Header({openLogin}: HeaderProps) {
 
     function signout(): void {
         logout();
+        localStorage.clear();
         navigate("/");
     }
 
     return(
         <header className="w-full h-15 flex items-center justify-between px-5 border">
-            <div className="w-full flex items-center justify-evenly">
-                <div className="flex gap-2 items-center">
-                    <Hexagon />
-                    <h4 className="text-xl font-bold">Study</h4>
+            <div className="min-w-4xl mx-auto">
+                <div className={`w-full flex items-center ${checkAuth() ? 'justify-between' : 'justify-evenly'}`}>
+                    <div className="flex gap-2 items-center">
+                        <Hexagon />
+                        <h4 className="text-xl font-bold">Study</h4>
+                    </div>
+                    {!checkAuth() &&                 
+                        <nav className="flex gap-3">
+                            <NavLink to={checkAuth() ? 'tasks' : '/'} className={({isActive}) => isActive? styles.active : styles.link}>Home</NavLink>
+                            <NavLink to="/sobre" className={({isActive}) => isActive? styles.active : styles.link}>Sobre</NavLink>
+                            <NavLink to="/contato" className={({isActive}) => isActive? styles.active : styles.link}>Contato</NavLink>
+                        </nav>
+                    }
+                    <Button size="sm" onClick={checkAuth()? signout : openLogin} className="w-[80px] cursor-pointer">{checkAuth()? 'Sair':'Login'}</Button>
                 </div>
-                <nav className="flex gap-3">
-                    <NavLink to={checkAuth() ? 'tasks' : '/'} className={({isActive}) => isActive? styles.active : styles.link}>Home</NavLink>
-                    <NavLink to="/sobre" className={({isActive}) => isActive? styles.active : styles.link}>Sobre</NavLink>
-                    <NavLink to="/contato" className={({isActive}) => isActive? styles.active : styles.link}>Contato</NavLink>
-                </nav>
-                <Button size="sm" onClick={checkAuth()? signout : openLogin} className="w-[80px] cursor-pointer">{checkAuth()? 'Sair':'Login'}</Button>
             </div>
         </header>
     )
