c8ce53db5d71107af449560ad5b4276ceac52e3a
Daniel Lima <130112273+maindan@users.noreply.github.com>
2025-09-30 17:31:28 -0400

    feat: updating in storage name of ProfileState.ts

--- src/states/ProfileState.ts
+++ src/states/ProfileState.ts
@@ -24,7 +24,7 @@ export const useProfileStore = create<ProfileState>()(
             }
         }),
         {
-            name: "auth-storage",
+            name: "profile-storage",
         }
     )
 )
