f6c0a529b94e6f2b7953c685f97ab42422241726
dan <danielkksilva1@gmail.com>
2025-10-26 18:28:34 -0400

    Adicionando pasta Task e arquivos

---
+++ src/components/Task/TaskComponent.tsx
@@ -0,0 +1,44 @@
+import type { ITask } from '@/interfaces/task'
+import { Check, Pause, Pencil, Play, Trash } from 'lucide-react'
+import React from 'react'
+import { Button } from '../ui/button'
+import "./task.css"
+
+type TaskProp = {
+  task: ITask;
+  onEdit: (id: number) => void;
+  onDelete: (id: number) => void;
+  onStartTask: (id: number) => void;
+  onFinishTask: (id: number) => void;
+}
+
+export function TaskComponent({task, onEdit, onDelete, onStartTask, onFinishTask}: TaskProp) {
+
+  function getStatusClass():string {
+    if(task.status == "PENDENTE") return "pending"
+    else if (task.status == "EM_ANDAMENTO") return "current"
+    return "finished";
+  }
+
+  function getStatus(): string {
+    return (task.status.charAt(0).toUpperCase() + task.status.toLocaleLowerCase().slice(1)).replaceAll("_", " ");
+  }
+
+  return (
+    <div className="min-h-30 rounded-lg p-3 shadow-effect-xl flex flex-col justify-between bg-[#191f36]">
+      <h2>{task.description}</h2>
+      <div className="flex w-full items-center justify-between">
+        <p className={`${getStatusClass()} } px-1 text-[12px] w-fit rounded-md h-5`}>{getStatus()}</p>
+        <div className="flex gap-2 justify-end">
+          {task.status === "PENDENTE" ? 
+            (<Button size="icon" className=" cursor-pointer rounded-4xl" onClick={() => onStartTask(task.id)}><Play/></Button>):
+            task.status === "EM_ANDAMENTO" ?
+            (<Button size="icon" className=" cursor-pointer rounded-4xl" onClick={() => onFinishTask(task.id)}><Check/></Button>):""
+          }
+          {task.status !== "CONCLUIDO" && (<Button size="icon" className=" cursor-pointer rounded-4xl" variant="ghost" onClick={() => onEdit(task.id)}><Pencil/></Button>)}
+          <Button size="icon" className=" cursor-pointer rounded-4xl" variant="destructive" onClick={() => onDelete(task.id)}><Trash/></Button>
+        </div>
+      </div>
+    </div>
+  )
+}
